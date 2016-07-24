270938802588f0e9f05278fc44a27174a09543b3
season <season@season-wang>
2016-07-24 16:12:00 +0800

    新增热门专题列表

--- src/com/car/training/action/TopicAction.java
+++ src/com/car/training/action/TopicAction.java
@@ -1,6 +1,8 @@
  package com.car.training.action;
 
- import org.ironrhino.core.metadata.AutoConfig;
+ import java.util.List;
+
+import org.ironrhino.core.metadata.AutoConfig;
 import org.ironrhino.core.model.ResultPage;
 import org.ironrhino.core.struts.BaseAction;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ public class TopicAction extends BaseAction {
 	private TopicService topicService;
 	/** 专题列表 */
 	private ResultPage<Topic> topicList;
+	/** 热点专题列表 */
+	private List<Topic> topicHotList;
 	/** 页大小 */
 	private Integer pageSize;
 	/** 页号 */
@@ -28,6 +32,9 @@ public class TopicAction extends BaseAction {
 		//专题列表(包含分页)
 		topicList = topicService.findPageByTopic(new Topic(), pageSize, pageNo);
 		
+		//热门专题列表(包含分页)
+		topicHotList =topicService.findListByIndexTopic(8);
+		
 		return SUCCESS;
 	}
 
@@ -37,6 +44,14 @@ public class TopicAction extends BaseAction {
 
 	public void setTopicList(ResultPage<Topic> topicList) {
 		this.topicList = topicList;
+	}
+
+	public List<Topic> getTopicHotList() {
+		return topicHotList;
+	}
+
+	public void setTopicHotList(List<Topic> topicHotList) {
+		this.topicHotList = topicHotList;
 	}
 
 	public Integer getPageSize() {
@@ -54,6 +69,4 @@ public class TopicAction extends BaseAction {
 	public void setPageNo(Integer pageNo) {
 		this.pageNo = pageNo;
 	}
-
-
 }
