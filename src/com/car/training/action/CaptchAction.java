4b31483686e65e21d8ad5f690120b03fc766cb0c
season <season@season-wang>
2016-07-24 14:44:53 +0800

    生成验证码Action

---
+++ src/com/car/training/action/CaptchAction.java
@@ -0,0 +1,50 @@
+package com.car.training.action;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+import org.ironrhino.core.metadata.AutoConfig;
+import org.ironrhino.core.struts.BaseAction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.car.training.service.CaptchManager;
+
+@AutoConfig
+public class CaptchAction extends BaseAction {
+
+	private static final long serialVersionUID = -2872618258899872140L;
+
+	@Autowired
+	private transient CaptchManager captchManager;
+
+	private String token;
+
+	public String captimg() {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		HttpServletRequest request = ServletActionContext.getRequest();
+
+		response.setContentType("image/jpeg");
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+
+		try {
+			ImageIO.write(captchManager.refreshCode(request.getSession().getId()), "JPEG", response.getOutputStream());
+			response.getOutputStream().close();
+			ServletActionContext.getContext().getActionInvocation().getProxy().setExecuteResult(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
