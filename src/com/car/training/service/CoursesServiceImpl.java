a1863d845d4fa206c13c6d701d4119f9eadc7897
season <season@season-wang>
2016-07-25 14:30:59 +0800

    xxx

--- src/com/car/training/service/CoursesServiceImpl.java
+++ src/com/car/training/service/CoursesServiceImpl.java
@@ -2,7 +2,6 @@ package com.car.training.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.ironrhino.common.model.Region;
 import org.ironrhino.core.aop.Timing;
