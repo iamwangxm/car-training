f15610c28b277ab46cda66c4ab8e35fc42988ad6
season <season@season-wang>
2016-07-31 22:37:21 +0800

    评论

--- src/com/car/training/action/website/TopicDetailAction.java
+++ src/com/car/training/action/website/TopicDetailAction.java
@@ -1,11 +1,15 @@
  package com.car.training.action.website;
 
- import org.apache.commons.lang3.StringUtils;
+ import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.ironrhino.core.metadata.AutoConfig;
 import org.ironrhino.core.struts.BaseAction;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.car.training.domain.Topic;
+import com.car.training.domain.TopicComment;
+import com.car.training.service.TopicCommentService;
 import com.car.training.service.TopicService;
 
 
@@ -16,8 +20,14 @@ public class TopicDetailAction extends BaseAction {
 
 	@Autowired
 	private TopicService topicService;
-
+	@Autowired
+	private TopicCommentService topicCommentService;
+	
 	private Topic topic;
+	/** 首页培训学热点专题列表 */
+	private List<Topic> topicList;
+	/**评论列表*/
+	private List<TopicComment> topicCommentList;
 
 	@Override
 	public String execute() throws Exception {
@@ -26,6 +36,13 @@ public class TopicDetailAction extends BaseAction {
 			t.setId(topic.getId());
 		}
 		topic = topicService.findById(t.getId());
+		if(topic!=null){
+			TopicComment topicComment = new TopicComment();
+			topicComment.setTopic(topic);
+			topicCommentList = topicCommentService.findListByTopicComment(topicComment);
+		}
+		//首页热点专题列表6个位置
+		topicList = topicService.findListByIndexTopic(6);
 		
 		return SUCCESS;
 	}
@@ -36,6 +53,22 @@ public class TopicDetailAction extends BaseAction {
 
 	public void setTopic(Topic topic) {
 		this.topic = topic;
+	}
+
+	public List<Topic> getTopicList() {
+		return topicList;
+	}
+
+	public void setTopicList(List<Topic> topicList) {
+		this.topicList = topicList;
+	}
+
+	public List<TopicComment> getTopicCommentList() {
+		return topicCommentList;
+	}
+
+	public void setTopicCommentList(List<TopicComment> topicCommentList) {
+		this.topicCommentList = topicCommentList;
 	}
 
 
